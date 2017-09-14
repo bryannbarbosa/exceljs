3e4f9595c4bf6d1db89005f5f5b53d75395acada
Bryann <bryannbarbosa@gmail.com>
2017-09-14 19:14:42 -0300

    new implementations

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Excel = require('exceljs');
-let filename = './acao_today_works_18000.xlsx';
+let filename = './acao_today_works_18000_cp.xlsx';
 var workbook = new Excel.Workbook();
 workbook.xlsx.readFile(filename)
     .then(function() {
@@ -10,9 +10,16 @@ workbook.xlsx.readFile(filename)
         for(let i = 1; i <= worksheet.rowCount; i++) {
           let row = worksheet.getRow(i);
           let value = row.getCell(1).value.toString();
-          //value = value.trim();
-          //value = value.replace(/\s/g, '');
+          value = value.trim();
+          value = value.replace(/\s/g, '');
+          value = value.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+          row.getCell(1).value = value;
+          row.commit();
           let length = row.getCell(1).value.toString().length;
+
+          if(value == '1184116446') {
+            console.log('found');
+          }
           
           if(length <= 7) {
             //console.log(value);
@@ -20,29 +27,31 @@ workbook.xlsx.readFile(filename)
           }
           if(length == 8 && arr.indexOf(Number(value.substr(0,2))) > -1) {
             //console.log('55' + '11' + value);
-           // console.log('55' + '11' + '9' + value);
-           // row.getCell(1).value = '55' + '11' + '9' + value;
+           row.getCell(1).value = '55' + '11' + value;
           }
           else if(length == 8 && !arr.indexOf(Number(value.substr(0,2))) > -1) {
             //console.log('55' + '11' + '9' + value);
+            row.getCell(1).value = '55' + '11' + '9' + value;
           }
 
           if(length == 9) {
            // console.log('55' + '11' + value);
-           // row.getCell(1).value = '55' + '11' + value;
+           row.getCell(1).value = '55' + '11' + value;
           }
 
           if(length == 10 && arr.indexOf(Number(value.substr(2,2))) > -1) {
             //console.log('55' + value);
-            //row.getCell(1).value = '55' + value;
+            row.getCell(1).value = '55' + value;
           }
 
           else if(length == 10 && !arr.indexOf(Number(value.substr(2,2))) > -1) {
+            //console.log('55' + sub);
             let sub = value.substr(0,2) + '9' + value.substr(2);
-            console.log('55' + sub);
+            row.getCell(1).value = '55' + sub;
           }
           if(length == 11) {
-            console.log('55' + value);
+            //console.log('55' + value);
+            row.getCell(1).value = '55' + value;
           }
           row.commit();
         }
