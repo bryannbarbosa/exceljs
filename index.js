480d1bafc64a30a1a3feab8f5e11f5e0f155fde5
Bryann <bryannbarbosa@gmail.com>
2017-09-14 21:37:37 -0300

    working example

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Excel = require('exceljs');
-let filename = './acao_today_works_18000_cp.xlsx';
+let filename = './acao_today_works_18000.xlsx';
 var workbook = new Excel.Workbook();
 workbook.xlsx.readFile(filename)
     .then(function() {
@@ -17,14 +17,20 @@ workbook.xlsx.readFile(filename)
           row.commit();
           let length = row.getCell(1).value.toString().length;
 
-          if(value == '1184116446') {
-            console.log('found');
-          }
+          //if(value == '1184116446') {
+           // console.log('found');
+          //  let sub = row.getCell(1).toString;
+          //  console.log('found');
+         // }
+
           
           if(length <= 7) {
+            row.getCell(1).value = 0;
             //console.log(value);
-            worksheet.spliceRows(i, 1);
+            //worksheet.spliceRows(i, 1);
           }
+
+          
           if(length == 8 && arr.indexOf(Number(value.substr(0,2))) > -1) {
             //console.log('55' + '11' + value);
            row.getCell(1).value = '55' + '11' + value;
@@ -47,11 +53,24 @@ workbook.xlsx.readFile(filename)
           else if(length == 10 && !arr.indexOf(Number(value.substr(2,2))) > -1) {
             //console.log('55' + sub);
             let sub = value.substr(0,2) + '9' + value.substr(2);
-            row.getCell(1).value = '55' + sub;
+            /*if(i == 15936) {
+              console.log(row.getCell(1).value);
+              row.getCell(1).value = '55' + sub.toString();
+              console.log(row.getCell(1).value);
+              //console.log('55' + sub);
+            }*/
+              row.getCell(1).value = '55' + sub.toString();
+              //console.log('55' + sub);
+           // let sub = value.substr(0,2) + '9' + value.substr(2);
+            //row.getCell(1).value = '55' + sub;
           }
           if(length == 11) {
-            //console.log('55' + value);
-            row.getCell(1).value = '55' + value;
+           
+            row.getCell(1).value = '55' + row.getCell(1).value.toString();
+          }
+          if(length == 12 && !arr.indexOf(Number(value.substr(2,2))) > -1) {
+            let sub = '55' + value.slice(0, -1).toString();
+            row.getCell(1).value = sub;
           }
           row.commit();
         }
